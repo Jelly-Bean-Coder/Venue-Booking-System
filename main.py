0356f5187f57e12e052bc5b1abf37b1d7a81469e
Jelly-Bean-Coder <101370187+Jelly-Bean-Coder@users.noreply.github.com>
2025-07-26 11:57:51 -0600

    Update main.py
    Made the create_table() function

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import sqlite3
 db_conn = sqlite3.connect('bookings.db')
 db = db_conn.cursor()
+def create_table(table_name: str, **kwargs): # Pass colums as [COLUMN_NAME]=[DATA_TYPE]
+    db.execute(f"""CREATE TABLE IF NOT EXISTS {table_name}(
+            id INTEGER PRIMARY KEY
+        )""")
+    for name, type in kwargs.items():
+        db.execute(f"""ALTER TABLE {table_name}
+                    ADD {name} {type};""")
